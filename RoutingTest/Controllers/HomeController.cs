1de5724f7037ed4e345ae10cc22b6447f17f199c
serenata-keith-mifsud <keith.mifsud@serenataflowers.com>
2019-12-20 10:58:07 +0100

    Add project files.

---
+++ RoutingTest/Controllers/HomeController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace RoutingTest.Controllers
+{
+    public class HomeController : Controller
+    {
+        //[Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [ActionName("hello")]
+        [HttpGet("hello", Name = "Hello")]
+        public IActionResult AjaxCall()
+        {
+            return Ok("ok");
+        }
+    }
+}
